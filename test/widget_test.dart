544fd9a8b5d167723b6425747c7ae51e1f101ee9
Eth4ne9 <125377795+Eth4ne9@users.noreply.github.com>
2026-05-21 22:05:52 +0200

    feat: update widget test to include additional text expectations and add test dependencies

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,11 @@ void main() {
   testWidgets('shows the initial home page', (tester) async {
     await tester.pumpWidget(const MainApp());
 
-    expect(find.text('Hello World!'), findsOneWidget);
+    expect(find.text('Email'), findsOneWidget);
+    expect(find.text('Mot de passe'), findsOneWidget);
+    expect(find.text('Connexion'), findsOneWidget);
+    expect(find.text('Mot de passe oublié'), findsOneWidget);
+    expect(find.text('Créer un compte'), findsOneWidget);
     expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
